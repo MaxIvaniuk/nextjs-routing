b8379d1b6f426fa540e98b83318fe45384d493ee
WhatAbout <71294722+MaxIvaniuk@users.noreply.github.com>
2024-09-05 15:17:27 +0300

    add logic to navlink and separate it as client component

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,56 +1,66 @@
-import NewsList from "@/components/news-list";
-import { 
-    getAvailableNewsMonths, 
-    getAvailableNewsYears, 
-    getNewsForYear, 
-    getNewsForYearAndMonth 
-} from "@/lib/news";
-import Link from "next/link";
+import Link from 'next/link';
 
-export default function FilteredNewsPage({params}){
-    const filter = params.filter;
+import NewsList from '@/components/news-list';
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from '@/lib/news';
 
-    const selectedYear = filter?.[0]
-    const selectedMounth = filter?.[1]
+export default function FilteredNewsPage({ params }) {
+  const filter = params.filter;
 
-    let news;
-    let links = getAvailableNewsYears();
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
 
-    if(selectedYear && !selectedMounth) {
-        news = getNewsForYear(selectedYear);
-        links = getAvailableNewsMonths(selectedYear);
-    }
+  let news;
+  let links = getAvailableNewsYears();
 
-    if(selectedYear & selectedMounth) {
-        news = getNewsForYearAndMonth(selectedYear, selectedMounth)
-    }
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
 
-    let newsContent = <p>No news found for the selected period.</p>
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
 
-    if(news && news.length > 0) {
-        newsContent = <NewsList news={news}/>
-    }
+  let newsContent = <p>No news found for the selected period.</p>;
 
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
 
-    return (
-        <>
-            <header id="archive-header">
-                <nav>
-                    <ul>
-                        {links.map(link => {
-                            const href = selectedYear 
-                                ? `/archive/${selectedYear}/${link}`
-                                : `/archive/${link}`
-                            return (
-                                <li key={link}>
-                                    <Link href={href}>{link}</Link>
-                                </li>
-                            )
-                        })}
-                    </ul>
-                </nav>
-            </header>
-            {newsContent}
-        </>
-    )
-} 
+  if (
+    (selectedYear && !getAvailableNewsYears().includes(+selectedYear)) ||
+    (selectedMonth &&
+      !getAvailableNewsMonths(selectedYear).includes(+selectedMonth))
+  ) {
+    throw new Error('Invalid filter.');
+  }
+
+  return (
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
+  );
+}
