665ce7db72a5b30a547898a80f15fa6fead33995
WhatAbout <71294722+MaxIvaniuk@users.noreply.github.com>
2024-10-23 14:14:14 +0300

    finish migrating app to local db instead of dummy-news file

--- app/(content)/news/[slug]/image/page.js
+++ app/(content)/news/[slug]/image/page.js
@@ -3,7 +3,7 @@ import { getNewsItem } from "@/lib/news"
 
 export default async function ImagePage({ params }) {
     const newsItemSlug = params.slug
-    const newsItem = await getNewsItem(slug)
+    const newsItem = await getNewsItem(newsItemSlug)
 
     if (!newsItem) notFound()
 
